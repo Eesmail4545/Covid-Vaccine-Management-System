7718907cc9f6a0afeb5d91488b43387f8c8754c6
Ismail <felewany03@gmail.com>
2023-02-24 16:22:29 +0300

    first commit

---
+++ new1.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>VAXSYM</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <link rel= "stylesheet" href="/CVMS/CSS/homepage.css" >
+  </head>
+<html class="w3-black">
+
+
+<!-- Page Content -->
+<body  >
+<div class="homepagebody">
+<div class="navbardiv">
+ <p class="logo" >VAXSYM</p>
+  <ul>
+
+    <li><a href="/CVMS/new1.php">Home</a></li>
+    <li ><a href='https://news.google.com/covid19/map?hl=en-KE&mid=%2Fm%2F019rg5&gl=KE&ceid=KE%3Aen'>CovidStats</a></li>
+    <li ><a  href="/CVMS/help.html">Help</a></li>
+    <li ><a href="/CVMS/dummy/reg.php">Register</a></li>
+    <li ><a href="/CVMS/dummy/login.php">Login</a></li>
+    <li><a href="/CVMS/dummy/profile1.php">Myprofile</a></li>
+    <li> <a href="/CVMS/dummy/logoutpage.php">logout</a></li>
+
+
+  </ul>
+</div>
+<div class="homepagecontent">
+  <h1>Immunization Saves Lives, Chanjo Inaokoa Maisha<br> Get vaccinated Today</h1>
+  <p>By getting vaccinated, we are not only protecting ourselves, but also those around us,<br> particularly the most vulnerable among us who cannot get vaccinated themselves.</p>
+</div>
+</body>
+</html>
